b3cc1e6062f537a2563d7db882500ab8bcd40f79
dimka59ru <dimka59ru@gmail.com>
2017-05-20 23:59:07 +0500

    save changes

--- programm.py
+++ programm.py
@@ -2,7 +2,7 @@
 from PyQt5 import QtGui
 
 from PyQt5.QtCore import QDateTime, QDate
-from PyQt5.QtGui import QIcon, QPixmap, QPalette, QColor
+from PyQt5.QtGui import QIcon, QPixmap, QPalette, QColor, QDoubleValidator
 from PyQt5.QtWidgets import QDialog, QLineEdit, QPushButton, QVBoxLayout, QMessageBox, QMainWindow, QApplication, \
     QLabel, QWidget, QAction, qApp, QAbstractItemView, QTableWidgetItem, QHeaderView, QTableWidget
 # from mainwindow import Ui_MainWindow
@@ -75,25 +75,25 @@ class Window(QMainWindow):
             # Создание курсора
             self.cur_db = self.conn.cursor()
 
-
-
             # Инициализация таблиц
             self.column_name_income = ["Статья доходов", "Действительна до"]
             self.column_name_costs = ["Статья расходов", "Действительна до"]
-            self.column_name_table_records_incomes = ["Дата занесения", "Статья", "Сумма"]
+            self.column_name_table_records = ["Дата занесения", "Статья", "Сумма"]
             # self.column_name_table_records_costs = ["Дата занесения", "Статья расхода", "Сумма"]
             self.init_table(self.table_incomes, self.column_name_income)
             self.init_table(self.table_costs, self.column_name_costs)
-            self.init_table(self.table_records_incomes, self.column_name_table_records_incomes)
+            self.init_table(self.table_records, self.column_name_table_records)
             # self.init_table(self.table_records_costs, self.column_name_table_records_costs)
 
-            # Получение данных из БД
+            # Обновление информации в окне
             self.update_data_in_ui()
-
 
             # Нажатие кнопки добавления статьи доходов
             self.button_add_item_income.clicked.connect(self.press_button_add_item_income)
             self.button_add_item_costs.clicked.connect(self.press_button_add_item_costs)
+
+            self.button_add_income.clicked.connect(self.press_button_add_income)
+            self.button_add_cost.clicked.connect(self.press_button_add_cost)
 
             #  Установим в поле с датой действия дату + год вперед
             self.set_date_in_date_edit(self.date_edit_add_item_income)
@@ -108,6 +108,12 @@ class Window(QMainWindow):
 
             self.get_data_records()
 
+            validator = QDoubleValidator(0.00, 999999.99, 2)
+            validator.setNotation(QDoubleValidator.StandardNotation)
+            self.line_sum_incomes.setValidator(validator)
+            self.line_sum_costs.setValidator(validator)
+
+
 
         except FileNotFoundError:
             QMessageBox.warning(
@@ -120,19 +126,17 @@ class Window(QMainWindow):
         self.data_incomes = self.get_data_incomes()
         self.data_costs = self.get_data_costs()
 
-        self.data_records = self.get_data_records()
+        self.income_records, self.costs_records = self.get_data_records()
 
         # Вывод статей в таблицы
         self.write_in_table(self.data_incomes, self.table_incomes)
         self.write_in_table(self.data_costs, self.table_costs)
 
-        self.write_in_table_records(self.data_records, self.table_records_incomes)
+        self.write_in_table_records(self.income_records, self.costs_records, self.table_records)
 
         # Заполнение выпадающих списков статей
         self.write_in_combobox(self.data_incomes, self.combobox_incomes)
         self.write_in_combobox(self.data_costs, self.combobox_costs)
-
-
 
     def set_date_in_date_edit(self, date_edit):
         """ Установит в поле с датой действия дату + год вперед """
@@ -194,6 +198,15 @@ class Window(QMainWindow):
                 self.update_data_in_ui()
         self.line_edit_add_item_costs.clear()
 
+    def press_button_add_income(self):
+        print("press_button_add_income")
+        print(self.combobox_incomes.currentText())
+        print(self.line_sum_incomes.text())
+
+    def press_button_add_cost(self):
+        print("press_button_add_cost")
+        print(self.combobox_costs.currentText())
+
     # Функция добавления статьи доходов в БД
     def add_item_income(self, name, expiration_date):
         try:
@@ -249,13 +262,18 @@ class Window(QMainWindow):
 
     def get_data_records(self):
         try:
-            self.cur_db.execute('SELECT * FROM financial_records WHERE del=0')
-            return self.cur_db.fetchall()
+            self.cur_db.execute('SELECT * FROM income_records WHERE del=0')
+            income_records = self.cur_db.fetchall()
+
+            self.cur_db.execute('SELECT * FROM costs_records WHERE del=0')
+            costs_records = self.cur_db.fetchall()
+
+            return income_records, costs_records
+
             # print(self.cur_db.fetchall())
         except sqlite3.DatabaseError as err:
             QMessageBox.warning(
                 self, 'Error', '#6 Error db: {}'.format(err))
-
 
     # Функция вывода записей в таблице статей доходов/расходов
     def write_in_table(self, data, table):
@@ -269,43 +287,104 @@ class Window(QMainWindow):
                 table.setItem(inx, 1, QTableWidgetItem("Бессрочно"))
             else:
                 table.setItem(inx, 1, QTableWidgetItem(str(self.convert_date(row[2]))))
-        table.resizeColumnsToContents()
 
-    def write_in_table_records(self, data, table):
+        table.resizeColumnsToContents()
+        table.horizontalHeader().setStretchLastSection(True)
+
+    def write_in_table_records(self, income_records, costs_records, table):
         table.clearContents()
         table.setRowCount(0)
-        for row in data:
-            inx = data.index(row)
-            table.insertRow(inx)
-            table.setItem(inx, 0, QTableWidgetItem(str(self.convert_date(row[1]))))
-            table.setItem(inx, 2, QTableWidgetItem(str(row[3])))
 
-            if row[2] == "plus":
+        # print("!")
+        # zipped = zip(income_records, costs_records)
+        # print(list(zipped))
+        all = []
+        for row in income_records:
+            temp = []
+            temp.append(1)
+            temp.append(row)
+
+            all.append(temp)
+        # print(all)
+        for row in costs_records:
+            temp = []
+            temp.append(2)
+            temp.append(row)
+            all.append(temp)
+
+        all.sort(key=self.sort_by_date, reverse=True)
+
+
+        for inx, row in enumerate(all):
+
+            table.insertRow(inx)
+            table.setItem(inx, 0, QTableWidgetItem(str(self.convert_date(row[1][1]))))
+
+            table.setItem(inx, 2, QTableWidgetItem(str(row[1][2])))
+
+            table.setItem(inx, 1, QTableWidgetItem(str(row[0])))
+
+            if row[0] == 1:
                 for row2 in self.data_incomes:
-                    if row[4] == row2[0]:
+                    if row[1][3] == row2[0]:
                         table.setItem(inx, 1, QTableWidgetItem(str(row2[1])))
-                # table.item(inx, 1).setBackground(QtGui.QColor("red"))
+
                 self.set_color(table, "#e5fbe5", inx)
-            elif row[2] == "minus":
+            elif row[0] == 2:
                 for row2 in self.data_costs:
-                    if row[4] == row2[0]:
+                    if row[1][3] == row2[0]:
                         table.setItem(inx, 1, QTableWidgetItem(str(row2[1])))
+
                 self.set_color(table, "#ffd8d8", inx)
 
+                # for item in row[1]:
+                #
+                #     print(item)
+                # table.setItem(inx, 0, QTableWidgetItem(str(self.convert_date(item[1]))))
+
+                # for lst in (income_records, costs_records):
+                #
+                #
+                #     for row in lst:
+                #         print(row)
+                # inx = lst.index(row)
+                # table.insertRow(inx)
+                # table.setItem(inx, 0, QTableWidgetItem(str(self.convert_date(row[1]))))
+                # table.setItem(inx, 1, QTableWidgetItem(str(row[3])))
+                # table.setItem(inx, 2, QTableWidgetItem(str(row[2])))
+
+        # for row in data:
+        #     inx = data.index(row)
+        #     table.insertRow(inx)
+        #     table.setItem(inx, 0, QTableWidgetItem(str(self.convert_date(row[1]))))
+        #     table.setItem(inx, 2, QTableWidgetItem(str(row[3])))
+        #
+        #     if row[2] == "plus":
+        #         for row2 in self.data_incomes:
+        #             if row[4] == row2[0]:
+        #                 table.setItem(inx, 1, QTableWidgetItem(str(row2[1])))
+        #         # table.item(inx, 1).setBackground(QtGui.QColor("red"))
+        #         self.set_color(table, "#e5fbe5", inx)
+        #     elif row[2] == "minus":
+        #         for row2 in self.data_costs:
+        #             if row[4] == row2[0]:
+        #                 table.setItem(inx, 1, QTableWidgetItem(str(row2[1])))
+        #         self.set_color(table, "#ffd8d8", inx)
+
         table.resizeColumnsToContents()
+        table.horizontalHeader().setStretchLastSection(True)
 
-            # # table.setColumnWidth(1, QHeaderView.ResizeToContents)
-            # table2 = QTableWidget
-            # table2.resizeColumnsToContents()
-
-
+    def sort_by_date(self, key):
+        return key[1][1]
 
     def set_color(self, table, color, inx):
-        for i, c in enumerate(self.column_name_table_records_incomes):
+
+        for i, c in enumerate(self.column_name_table_records):
             table.item(inx, i).setBackground(QtGui.QColor(color))
 
 
-        # table.item(inx, 1).setBackground(QtGui.QColor(100, 100, 150))
+            # table.item(inx, 1).setBackground(QtGui.QColor(100, 100, 150))
+
     # Функция вывода записей в комбобоксы
     def write_in_combobox(self, data, combobox):
         combobox.clear()
